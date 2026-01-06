f5eec15b76828774d9c703869965f285247afbae
rajeswari235 <balarajeswari1910@gmail.com>
2026-01-06 15:04:26 +0530

    initial commit

---
+++ mainwindow.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "xlsxdocument.h"
+#include <QStandardPaths>
+#include <QLabel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_pushButton_upload_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+
+    int totalQuantity=0;
+    double totalAmount=0.0;
+
+
+};
+
+#endif // MAINWINDOW_H
